e09698d7c956ebd93f820fd623d092726e776957
Kent Ian Orcales <orcaleskeiano@gmail.com>
2024-10-12 16:27:09 +0800

    X689B: Initial TeamWin tree

---
+++ Android.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE), Infinix-X689B)
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+endif
